6d04dfa927994a4548d9eb569b673e2bf9bff09e
Ray Villalobos <ray@planetoftheweb.com>
2019-12-19 18:08:24 -0800

    added a series of commands

--- code/scripts.js
+++ code/scripts.js
@@ -2,7 +2,10 @@ const vscode = require("vscode");
 
 function activate(context) {
   let disposable = vscode.commands.registerCommand('doSomething', function() {
-    vscode.window.showInformationMessage('This is a disposable window');
+    vscode.window.showInformationMessage('Copied text, commented out lines, preview markdown file');
+    vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+    vscode.commands.executeCommand('editor.action.addCommentLine');
+    vscode.commands.executeCommand('markdown.showPreviewToSide');
   })
 
   context.subscriptions.push(disposable)
